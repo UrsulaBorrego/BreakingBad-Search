5fb8a147491f99a282882714df809ef5104f1a21
Ursula Borrego <ursulaborrego@hotmail.com>
2022-11-12 18:15:22 +0000

    Show all characters. Filter in input for name.

--- src/js/main.js
+++ src/js/main.js
@@ -1,42 +1,3 @@
-// 'use strict';
-
-// //QUERYSELECTORS
-
-// const btn = document.querySelector('.js-button-search');
-// const characterList = document.querySelector('.js-characters-list');
-
-// //VARIABLES GLOBALES
-
-// const oneCharacter = {
-//     id: 1,
-//     name: 'Walter White',
-//     photo: 'https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg',
-//     status: 'Presumed dead',
-// };
-
-// //FUNCIONES
-
-// //Función que pinta cada personaje
-// function renderOneCharacter() {
-// characterList.innerHTML = 
-// `<li>
-// <article class="character selected">
-//   <img src=${oneCharacter.photo} alt="" class="photo">
-//   <h3 class="name">${oneCharacter.name}</h3>
-//   <p class="status">${oneCharacter.status}</p>
-// </article>
-// </li>`;
-// };
-
-// //ADDEVENTLISTENERS
-
-// //CÓDIGO QUE SE EJECUTA AL CARGAR LA PÁGINA
-// renderOneCharacter();
-
-
-
-
-///////////EN BASE AL EJERCICIO CAMISETAS 
 'use strict';
 
 //QUERYSELECTORS
@@ -46,62 +7,65 @@ const inputSearch = document.querySelector('.js-input-search');
 //Botón Search
 const btnSearch = document.querySelector('.js-button-search');
 //Ul de los personajes
-const characterList = document.querySelector('.js-characters-list');
-
+let characterList = document.querySelector('.js-characters-list');
 
 //VARIABLES GLOBALES (vacías, para rellenar con API)
 
 //Personajes
 let allCharacters = [];
 //Personajes favoritos
-let favouriteCharacters = [];
+// let favouriteCharacters = [];
+//Tarjeta de cada personaje
+let characterCard = '';
+//Recoje el valor del input
+let textInput = '';
 
-//FUNCIONES
 
-// //Función que coge las variables del API
-// const getCharacterHtmlCode = (character) => {
-//     let htmlCode = '';
-//     `<li>
-//     <article class="character selected">
-//     <img src=${oneCharacter.photo} alt="" class="photo">
-//     <h3 class="name">${oneCharacter.name}</h3>
-//     <p class="status">${oneCharacter.status}</p>
-//     </article>
-//     </li>`;
-// };
+//Función para pintar los personajes
+const renderCharacters = () => {
+  let characterCardContent = '';
+  characterCard = '';
+  characterList.innerHTML = '';
+  //Bucle para recorrer el API
+  for (const character of allCharacters) {
+    characterCardContent = `<li>
+        <article class="character selected">
+        <img src='${character.img}' alt="" class="photo">
+        <h3 class="name">${character.name}</h3>
+        <p class="status">${character.status}</p>
+        </article>
+        </li>`;
+    //Meto el contenido en cada tarjeta
+    characterCard += characterCardContent;
+    //Pinto las tarjetas en el ul
+    characterList.innerHTML = characterCard;
+  }
+};
 
 //Función para traer los datos de los personajes del API
 const getApiData = () => {
-    fetch('https://breakingbadapi.com/api/characters')
-    .then(response => response.json())
-    .then(data => {
-        allCharacters = data; //TRAE BIEN LOS PERSONAJES DE LA API
-        characterList.innerHTML = JSON.stringify(allCharacters); //ME PINTA TODOS LOS OBJETOS
-        // paintCharacters();
-        console.log(characterList);
+  allCharacters= [];
+  fetch('https://breakingbadapi.com/api/characters')
+    .then((response) => response.json())
+    .then((data) => {
+      allCharacters.push(...data); //Agrego un array a otro array
+      //characterList.innerHTML = JSON.stringify(allCharacters); //ME PINTA TODOS LOS OBJETOS
+      renderCharacters();
     });
 };
 
-//Función para pintar los personajes      /////REVISAR ESTE FOR
-
-const paintCharacters = () => {
-
-
+//Función para buscar en el input
+const searchCharacter = (event) => {
+  event.preventDefault();
+  textInput = inputSearch.value;
+  //Filtro el contenido del input por nombre y pinto solo los filtrados
+  let filterResult = allCharacters.filter(character=>character.name.includes(textInput));
+  allCharacters = filterResult;
+  renderCharacters();
 };
 
-// const paintCharacters = () => {
-//     let charactersCode = '';
-//     for (const character of characters) {
-//         charactersCode += getCharacterHtmlCode(character);
-//     }
-//     characterList.innerHTML = charactersCode;
-// };
-
 //ADDEVENTLISTENERS
-
-
+btnSearch.addEventListener('click',searchCharacter);
 
 //CÓDIGO QUE SE EJECUTA AL CARGAR LA PÁGINA
-
 getApiData();
-// paintCharacters();
